4357099b737a84918fd98d349f9e911871ab964d
Andre W Silva <andre.desenvolvedor@gmail.com>
2020-02-12 15:00:01 -0300

    ...

--- src/main/java/br/com/andrewsilva/helpdesk/api/controllers/UserController.java
+++ src/main/java/br/com/andrewsilva/helpdesk/api/controllers/UserController.java
@@ -1,12 +1,21 @@
 package br.com.andrewsilva.helpdesk.api.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.andrewsilva.helpdesk.api.entity.User;
+import br.com.andrewsilva.helpdesk.api.responses.Response;
 import br.com.andrewsilva.helpdesk.api.service.UserService;
 
 @RestController
@@ -15,9 +24,23 @@ import br.com.andrewsilva.helpdesk.api.service.UserService;
 public class UserController {
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+
+	@PostMapping
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> create(HttpServletRequest request, @RequestBody User user,
+			BindingResult result) {
+		
+		Response<User> response = new Response<User>();
+		return null;
+	}
 	
-//	public ResponseEntity<Respo>
+	private void validateCreateUser(User user, BindingResult result) {
+		if(user.getEmail() == null) {
+			result.addError(new ObjectError("User", "Email no information"));
+			
+		}
+	}
 }
