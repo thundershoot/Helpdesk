e91a4fa64ef2f36b970820aced39801a22c5ee74
Andre W Silva <andre.desenvolvedor@gmail.com>
2020-02-17 17:53:37 -0300

    UserController

--- src/main/java/br/com/andrewsilva/helpdesk/api/controllers/UserController.java
+++ src/main/java/br/com/andrewsilva/helpdesk/api/controllers/UserController.java
@@ -1,18 +1,27 @@
 package br.com.andrewsilva.helpdesk.api.controllers;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.DuplicateKeyException;
 
 import br.com.andrewsilva.helpdesk.api.entity.User;
 import br.com.andrewsilva.helpdesk.api.responses.Response;
@@ -32,15 +41,102 @@ public class UserController {
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	public ResponseEntity<Response<User>> create(HttpServletRequest request, @RequestBody User user,
 			BindingResult result) {
-		
+
 		Response<User> response = new Response<User>();
-		return null;
-	}
-	
-	private void validateCreateUser(User user, BindingResult result) {
-		if(user.getEmail() == null) {
-			result.addError(new ObjectError("User", "Email no information"));
-			
+		try {
+			validateCreateUser(user, result);
+			if (result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			User userPersisted = (User) userService.createOrUpdate(user);
+			response.setData(userPersisted);
+		} catch (DuplicateKeyException e) {
+			response.getErrors().add("E-mail already registered");
+			return ResponseEntity.badRequest().body(response);
+		} catch (Exception e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
 		}
+		return ResponseEntity.ok(response);
+	}
+
+	private void validateCreateUser(User user, BindingResult result) {
+		if (user.getEmail() == null) {
+			result.addError(new ObjectError("User", "Email no information"));
+		}
+	}
+
+	@PutMapping
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> update(HttpServletRequest request, @RequestBody User user,
+			BindingResult result) {
+
+		Response<User> response = new Response<User>();
+		try {
+			validateUpdateUser(user, result);
+			if (result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			User userPersisted = (User) userService.createOrUpdate(user);
+			response.setData(userPersisted);
+		} catch (Exception e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+		return ResponseEntity.ok(response);
+	}
+
+	private void validateUpdateUser(User user, BindingResult result) {
+		if (user.getId() == null) {
+			result.addError(new ObjectError("User", "Id not informed"));
+		}
+		if (user.getEmail() == null) {
+			result.addError(new ObjectError("User", "Email not informed"));
+		}
+	}
+
+	@GetMapping(value = "{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> findById(@PathVariable("id") String id) {
+
+		Response<User> response = new Response<User>();
+		Optional<User> user = userService.findById(id);
+		if (user == null) {
+			response.getErrors().add("Register not foung by id: " + id);
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		response.setData(user.get());
+		return ResponseEntity.ok(response);
+	}
+
+	@DeleteMapping(value = "{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<String>> delete(@PathVariable("id") String id) {
+
+		Response<String> response = new Response<String>();
+		Optional<User> user = userService.findById(id);
+		if (user == null) {
+			response.getErrors().add("Register not foung by id: " + id);
+			return ResponseEntity.badRequest().body(response);
+
+		}
+
+		userService.delete(id);
+		return ResponseEntity.ok(new Response<String>());
+	}
+
+	@GetMapping(value = "{page}/{count}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<Page<User>>> findAll(@PathVariable("page") int page,
+			@PathVariable("count") int count) {
+		Response<Page<User>> response = new Response<Page<User>>();
+		Page<User> users = userService.findAll(page, count);
+		response.setData(users);
+		return ResponseEntity.ok(response);
 	}
 }
